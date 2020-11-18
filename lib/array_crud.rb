7a2e72a6f8681908bd53f4cf1bd07958e13cf187
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-18 19:12:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array = ["lil' bub", "grumpy cat"]
-  add_element_to_end_of_array << "nala cat"
+  add_element_to_end_of_array << "element"
 end
 
 def add_element_to_start_of_array(array, element)
