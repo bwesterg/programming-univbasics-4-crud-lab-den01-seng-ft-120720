68713730af9c3e2b39b27fe6ac7ae04632f5f57a
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-18 19:07:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,19 @@
 def create_an_empty_array
-  
+  create_an_empty_array = []
 end
 
 def create_an_array
-  
+  create_an_array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array = [1, 2, 3, 4]
+  add_element_to_end_of_array.push("A")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array = []
+  add_element_to_start_of_array.unshift = ["A"]
 end
 
 def remove_element_from_end_of_array(array)
