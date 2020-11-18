910b36f188978c51ef6199fb1efef07f0141e3c1
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-18 20:44:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,13 +19,13 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.remove_element_from_start_of_array
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array
 end
