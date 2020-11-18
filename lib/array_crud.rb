213efb3b2ece7a4df4c03b84f2bb7da12ac06c25
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-18 20:27:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,10 +1,9 @@
 def create_an_array
-  create_an_array = [1, 2, 3, 4]
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ["array"]
-  my_array << "element"
+ array << "element"
 end
 
 def add_element_to_start_of_array(array, element)
